6342275c0dec88a8a7977aa5c63ab3d57f28d93f
onkarsathe007 <onkarsathe96k@gmail.com>
2024-12-07 16:20:59 +0530

    Js fundamental's

---
+++ 4.JS/practice/app.js
@@ -0,0 +1,8 @@
+let pen = 10;
+let notebook = 50;
+// console.log("pen :"+pen)
+// above method is not a good method to write sentence so we use below method
+console.log(`pen price : ${pen}`);
+console.log(`Notebook price : ${notebook}`)
+console.log(`total price :${pen+notebook}`)
+// `` is known as a back tick,${} is template literal
